c6a174fd7c9398415b062d3557f8b5b8cd740894
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-01-16 21:00:39 +0545

    changed routes for administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
+      resources dashboard_resource
+    end
+
+    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
+  end
+
   root to: 'visitors#index'
   devise_for :users
 end
