2e7a1e8e2c05fc4f7f9ea1fa88e7e45ac69da963
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-01-18 22:01:53 +0545

    finished products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,50 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
+
+  # Private Methods #
+  private
+    def product_params
+      params.require(:product).permit(:title, :description, :price)
+    end
 end
