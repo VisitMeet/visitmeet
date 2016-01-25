00bdc8b9f9dab6d2a746094220ae5ab0957ea55a
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-01-26 00:13:06 +0545

    commented out user.password

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      # user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name #user model has name
       # user.image = auth.info.image #user have no image.
     end
