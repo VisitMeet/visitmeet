d0895e809274f6eaaf63e20e303750c98b423ed3
Will Schive <lexigram@goodworksonearth.org>
2016-01-29 23:18:29 -0800

    sign_out_spec test passing

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -3,7 +3,6 @@
 #   I want to sign out
 #   So I can protect my account from unauthorized access
 feature 'Sign out', :devise do
-
   # Scenario: User signs out successfully
   #   Given I am signed in
   #   When I sign out
@@ -12,10 +11,7 @@ feature 'Sign out', :devise do
     user = FactoryGirl.create(:user)
     signin(user.email, user.password)
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
-    click_link 'Sign out'
+    click_link 'Logout'
     expect(page).to have_content I18n.t 'devise.sessions.signed_out'
   end
-
 end
-
-
