9cc7bb12d41403228e91af993bd7349ff343c456
Will Schive <lexigram@goodworksonearth.org>
2016-01-29 23:15:15 -0800

    visitors about page tests pass

--- spec/features/visitors/about_page_spec.rb
+++ spec/features/visitors/about_page_spec.rb
@@ -3,14 +3,13 @@
 #   I want to visit an 'about' page
 #   So I can learn more about the website
 feature 'About page' do
-
   # Scenario: Visit the 'about' page
   #   Given I am a visitor
   #   When I visit the 'about' page
   #   Then I see "About the Website"
   scenario 'Visit the about page' do
-    visit 'pages/about'
+    visit '/pages/about'
+    expect(current_path).to eq '/pages/about'
     expect(page).to have_content 'About the Website'
   end
-
 end
