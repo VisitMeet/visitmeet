a3744d1fe16cf736cbb5fdc577d1c6722325a88f
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-01-30 22:46:28 +0545

    modified user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,23 +36,25 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
+
+
   def set_default_role
     self.role ||= :user
   end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.email = auth.info.email
-      # user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name #user model has name
       # user.image = auth.info.image #user have no image.
     end
   end
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  devise :omniauthable, :omniauth_providers => [:github]
 end
