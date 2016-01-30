2bf5aa1e72921d1e28f361f27929e8361bae8a26
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-01-30 22:45:00 +0545

    user will be redirected to their profile page after they signin with omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,18 +2,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     # Implement this in model.
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user  #, :event => :authentication #this will throw if @user is not activated
+    if user.persisted?
+      sign_in_and_redirect users_profile_path  #, :event => :authentication #this will throw if @user is not activated
       # set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+      flash[:notice] = "User signed in using Github"
       redirect_to products_path
 
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
+      flash[:notice] = "Complete the sign up using the form below."
       redirect_to new_user_registration_url
+      # redirect_to products_path
     end
-    flash[:notice] = "User signed in"
   end
 
   def failure
