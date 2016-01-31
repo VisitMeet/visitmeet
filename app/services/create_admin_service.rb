1fbf5e870f2c1eaa9bec82d7bbdd5a6e58ae0720
Will Schive <lexigram@goodworksonearth.org>
2016-01-31 11:38:13 -0800

    changes to run a fork

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,10 +1,10 @@
 class CreateAdminService
   def call
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
-        user.confirm!
-        user.admin!
-      end
+      user.password = Rails.application.secrets.admin_password
+      user.password_confirmation = Rails.application.secrets.admin_password
+      user.confirmed_at
+      user.admin!
+    end
   end
 end
