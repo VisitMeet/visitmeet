1898066564aec46293a6eb4decd942389acab76e
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-02-02 13:45:25 +0545

    added socialization gem and its features

---
+++ app/controllers/socializations_controller.rb
@@ -0,0 +1,27 @@
+class SocializationsController < ApplicationController
+  before_filter :load_socializable
+
+
+  def follow
+    current_user.follow!(@socializable)
+    render json: {follow: true}
+  end
+
+  def unfollow
+    current_user.unfollow!(@socializable)
+    render json: {follow: false}
+  end
+
+  private
+    def load_socializable
+      @socializable =
+        case
+        when id  = params[:user_id]
+          User.find(id)
+        else
+          raise ArgumentError, "Unsupported model, params:" +
+                                params.keys.inspect
+        end
+      raise ActiveRecord::RecordNotFound unless @socializable
+    end
+end
---
+++ app/helpers/socializations_helper.rb
@@ -0,0 +1,2 @@
+module SocializationsHelper
+end
