7854bbba7d22768ca80c36435a7571ded78b9daa
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-03-04 22:33:23 +0545

    added relation between user and profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,10 +29,14 @@
 #  invited_by_id          :integer
 #  invited_by_type        :string
 #  invitations_count      :integer          default(0)
+#  provider               :string
+#  uid                    :string
 #
 
 class User < ActiveRecord::Base
   has_many :products, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
