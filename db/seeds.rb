750d0715d2a86d719c1bf96908e551b945e13971
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-03-04 22:37:29 +0545

    added seed data for category and products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+# user = CreateAdminService.new.call
+# puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
-Product.create(title: 'Product 1', description: 'Description', price: 33, user_id: 1, category: 'Food')
-
-Product.create(title: 'Product 1', description: 'Description', price: 33, user_id: 1, category: 'Food')
-
-Product.create(title: 'Product 1', description: 'Description', price: 33, user_id: 1, category: 'Food')
-
-Product.create(title: 'Product 1', description: 'Description', price: 33, user_id: 1, category: 'Food')
-
-Product.create(title: 'Product 1', description: 'Description', price: 33, user_id: 1, category: 'Food')
+Category.create(name: 'Fooding')
+Category.create(name: 'Lodging')
+Category.create(name: 'Travelling')
+Category.create(name: 'Shopping')
+Product.create(title: "Fishing", description: "Fishing in home", price: 15)
+Product.create(title: "Paneer Pakoda with Tea", description: "Paneer Pakoda with Tea", price: 20)
+Product.create(title: "Local Chicken Curry", description: "Local Chicken Curry in Nepali Style", price: 25)
+Product.create(title: "Pork Kachol", description: "Steamed Pork mixed with Nepali masalas.", price: 30)
