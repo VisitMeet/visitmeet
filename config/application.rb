6fc7b5531598a0068483cd24fbfd04f4aeb47452
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-04-04 23:17:13 +0545

    removed frozen string literal

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 Rails.application.routes.draw do
   get 'profile/index'
 
