af947d2f40586c70d44c0ef24a0dd28e46e2941e
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-04-27 10:10:10 +0545

    comment out the mandrill specifics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,15 +74,15 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.mandrillapp.com',
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_apikey
-  }
+  #config.action_mailer.smtp_settings = {
+  # address: 'smtp.mandrillapp.com',
+  #  port: 587,
+  #  domain: Rails.application.secrets.domain_name,
+  #  authentication: 'plain',
+  #  enable_starttls_auto: true,
+  #  user_name: Rails.application.secrets.email_provider_username,
+  #  password: Rails.application.secrets.email_provider_apikey
+  # }
 
   # ActionMailer Config
   config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net' }
