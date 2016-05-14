87d5f63985422b67da2f8b33027684003c7f19ae
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-05-14 15:32:19 +0545

    changed defaut url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   # }
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net' }
+  config.action_mailer.default_url_options = { host: 'visitmeeet.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
