2e8cf92cbe269fb672c1d09beb7b8c76fcf63efe
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-06-19 21:34:15 +0545

    attr_accessible changed to attr_accessor

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,5 +5,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Rails.application.config.session_store :cookie_store, key: '_visitmeet_session'
-ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
+ActiveRecord::SessionStore::Session.attr_accessor :data, :session_id
 Rails.application.config.session_store :active_record_store
