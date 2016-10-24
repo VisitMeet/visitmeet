a77764fc6431e1c37e718231bc8f3c7dc2178e8c
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-10-24 22:27:34 +0545

    Fix Stripe api key configuration.
    Stripe is working successfully here locally.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,8 +7,8 @@
 
 
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_API_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
